bdb8624ddf57b87fe4bb1e2e8527126bcb98cf6b
camposmateus64-prog <camposmateus64@gmail.com>
2025-09-02 21:51:28 -0300

    criaçao do programa de tarefas

--- tarefas.py
+++ tarefas.py
@@ -1,4 +1,16 @@
-numero_1 = int(input("informe o numero // ->"))
-# soma = numero_1 * numero_2
-numero_1 = int(input("informe o primeiro numero ->"))
-numero_2 = int(input("informa o segundo numero->")
+# calculando o dobro numero informado
+numero_1 = int(input("informe um numero inteiro->"))
+print("o dobro do numero informe é ->", numero_1 * 2)
+
+# calculando a area do quadrado
+lado = float(input("imforme o valor do lado do quadrodo ->"))
+print("a area do quadrado é ->  ", lado * lado)             
+
+# calcula a idade em meses e dias
+idade = int(input("informe a sua idade ->"))
+meses = idade * 12
+dias = meses * 365 
+print("sua idade em meses é ", meses, "e em dias " , dias)
+
+nota1 = float(input("informe a primeira nota ->"))
+noat2 = float(input("informe a sengunda nota ->"))
