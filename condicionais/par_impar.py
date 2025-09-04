9d7f11c365eccdabdeb935ddaefb4c0d9cc3593a
camposmateus64-prog <camposmateus64@gmail.com>
2025-09-03 21:49:37 -0300

    inicio do conteudo comdicionas

---
+++ condicionais/par_impar.py
@@ -0,0 +1,6 @@
+numero = int(input("informe um mumero ->"))
+
+if(numero % 2) == 0:
+    print("o numero") 
+
+    print("o numero é par !")
---
+++ operadoresaritimetico/tarefa-2.py
@@ -0,0 +1,15 @@
+valor=int(input("informe um valor"))
+ant=1
+suc=1
+print(valor - ant )
+print ( valor + suc)
+
+# temperatura
+c=float(input("informe a temperatura °c ->"))
+f=float(c * 9 / 5) + 32
+print("temperatura em °f -> ", f)
+
+#calcula a pontencia
+numero_1= int(input("informe o primeiro numero ->"))
+numero_2= int(input("informe o segundo ->"))
+print("quociente inteiro -> ",  numero_1 // numero_2, "\n", "resto da divisão -> ", numero_1 % numero_2)
